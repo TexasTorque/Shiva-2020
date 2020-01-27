2e83d1cf95f17f8dbdb03552bae72e410da56610
swathimannem <s.mannem1300@gmail.com>
2020-01-27 16:30:51 -0600

    added jacob's color sensor stuff

--- src/main/java/org/texastorque/inputs/Feedback.java
+++ src/main/java/org/texastorque/inputs/Feedback.java
@@ -3,6 +3,7 @@ package org.texastorque.inputs;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 import org.texastorque.constants.*;
 import org.texastorque.util.TCS34725ColorSensor;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Feedback {
 
@@ -34,12 +35,44 @@ public class Feedback {
         // blue = colorSensor.getBlueVal();
     }
 
+    // ======== color sensor arduino =========
+
+    // private ArrayList<DigitalInput> rgbPins = new ArrayList<DigitalInput>();
+    private DigitalInput[] rgb = new DigitalInput[] {new DigitalInput(0), new DigitalInput(1), new DigitalInput(2)};
+    private String colorDetected = "";
+
+    public enum Color {
+        NONE, YELLOW, RED, GREEN, BLUE;
+    }
+    private Color color = Color.NONE;
+
+    public void updateColorSensor(){
+        if (rgb[2].get()){
+            color = Color.BLUE;
+        }
+        else if(!rgb[0].get() || rgb[1].get()){
+            color = Color.NONE;
+        } 
+        else if(rgb[0].get() && rgb[1].get()){
+            color = Color.GREEN;
+        }
+        else if(rgb[0].get()){
+            color = Color.YELLOW;
+        }
+        else {
+            color = Color.RED;
+        }   
+    } // update color sensor  
+
+    // ======== Other stuff =========
+    
     public void smartDashboard(){
         SmartDashboard.putNumber("red", red);
         SmartDashboard.putNumber("green", green);
         SmartDashboard.putNumber("blue", blue);
         SmartDashboard.putNumber("read", readTest);
         SmartDashboard.putNumber("init", init);
+        SmartDashboard.putString("Color", color.toString());
         
     } // stuff to put in smart dashboard
 
