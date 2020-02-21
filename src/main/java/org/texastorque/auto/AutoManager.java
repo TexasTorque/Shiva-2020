3feff5d0d4078a5564f806b7dd12c6e37cbc59a1
swathimannem <s.mannem1300@gmail.com>
2020-02-21 17:03:02 -0600

    testing

--- src/main/java/org/texastorque/auto/AutoManager.java
+++ src/main/java/org/texastorque/auto/AutoManager.java
@@ -13,38 +13,38 @@ public class AutoManager {
 
     private ArrayList<Sequence> autoSequences;
     private String[] autoSequenceNames;
-    // private SendableChooser<String> autoSelector = new SendableChooser<String>();
+    private SendableChooser<String> autoSelector = new SendableChooser<String>();
 
     private Sequence currentSequence;
     private boolean sequenceEnded;
 
     private AutoManager(){
         autoSequences = new ArrayList<Sequence>();
-        autoSequenceNames = new String[] {"Mode 0", "Baseline", "Mode 2", "Mode 3", "Testing"};
+        autoSequenceNames = new String[] {"Mode 0", "Baseline", "Mode 2", "Mode 3", "Mode 4", "Testing"};
         autoSequences.add(new VinayMode());
         autoSequences.add(new CrossLine());
         autoSequences.add(new ShootButWorse());
         autoSequences.add(new ShootButNotAsBad());
         autoSequences.add(new Testing());
 
-        SmartDashboard.putStringArray("Auto List", autoSequenceNames);
+        SmartDashboard.putStringArray("AutoList", autoSequenceNames);
         // System.out.println(working);
-        // autoSelector.setDefaultOption("VinayMode", "VinayMode");
-        // autoSelector.addOption("CrossLine", "CrossLine");
-        // autoSelector.addOption("Testing", "Testing");
+        autoSelector.setDefaultOption("VinayMode", "VinayMode");
+        autoSelector.addOption("CrossLine", "CrossLine");
+        autoSelector.addOption("Testing", "Testing");
 
-        // SmartDashboard.putData(autoSelector);
+        SmartDashboard.putData(autoSelector);
         System.out.println("All auto sequences loaded.");
     } // constructor
 
     public void displayChoices(){
-        // SmartDashboard.putData(autoSelector);
-        SmartDashboard.putStringArray("Auto List", autoSequenceNames);
+        SmartDashboard.putData(autoSelector);
+        SmartDashboard.putStringArray("AutoList", autoSequenceNames);
     } // display choices
 
     public void chooseSequence(){
-        // String autoChoice = autoSelector.getSelected();
-        String autoChoice = SmartDashboard.getString("Auto List", "null");
+        String autoChoice = autoSelector.getSelected();
+        // String autoChoice = SmartDashboard.getString("AutoList", "null");
         System.out.println(autoChoice);
         switch(autoChoice){
             default: // just change the value in here to test
